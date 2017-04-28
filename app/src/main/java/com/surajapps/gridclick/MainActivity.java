a889fbaa48a55d47ba3f9e8ed6850955b68b60dc
supreti17 <upretisuraj11@gmail.com>
2017-04-27 19:03:21 -0700

    Initial commit

---
+++ app/src/main/java/com/surajapps/gridclick/MainActivity.java
@@ -0,0 +1,36 @@
+package com.surajapps.gridclick;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(new GridButtonAdapter(getApplicationContext()));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    startActivity(new Intent(getApplicationContext(), RecyclerViewActivity.class));
+                } else {
+                    Toast.makeText(MainActivity.this, "Toast Button " + (position + 1), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
